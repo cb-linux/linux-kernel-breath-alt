518c44aa322e0a1831ab36aae619a40d77dd6e88
RunCros <lauret.andretheo@gmail.com>
2022-07-17 14:11:28 +0400

    initial

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#3 SMP Fri Jul 15 19:04:54 CEST 2022"
+#define LINUX_COMPILE_BY "lars"
+#define LINUX_COMPILE_HOST "andrelauret-lars"
+#define LINUX_COMPILER "gcc (Debian 10.2.1-6) 10.2.1 20210110, GNU ld (GNU Binutils for Debian) 2.35.2"
